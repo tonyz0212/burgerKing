b08b19d79dd75062d11d7ec471636c38f8fda2fa
tony <tonyz0212@gmail.com>
2020-08-06 17:25:59 -0700

    output ingredient dynamically

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css'
 
 const layout = (props) => (
     // having adjacent JSX element, we could use HOC to solve the issue. Another way to to use Array(But it needs the key attribute)
     <Aux>
         <div> Toolbar, Sidedraw , backdrop </div>
-        <main>
+        <main className = {classes.Content}>
             {props.children}
         </main>
     </Aux>
