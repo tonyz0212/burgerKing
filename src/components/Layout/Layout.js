66cda9a0d22aad3ecc7621972add96d80f0df6fb
tony <tonyz0212@gmail.com>
2020-08-08 11:34:45 -0700

    Added sidebar for phone users.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
-import classes from './Layout.module.css'
+import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-const layout = (props) => (
-    // having adjacent JSX element, we could use HOC to solve the issue. Another way to to use Array(But it needs the key attribute)
-    <Aux>
-        <Toolbar/>
-        {<SideDrawer /> }
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
 
-);
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
 
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 
-export default layout;
+export default Layout;
