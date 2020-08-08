11bd0fea9fe94894d1bef5b1ad4a19f3e40767b2
tony <tonyz0212@gmail.com>
2020-08-07 17:04:32 -0700

    Fixed the compability for phone view size.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
 const layout = (props) => (
     // having adjacent JSX element, we could use HOC to solve the issue. Another way to to use Array(But it needs the key attribute)
     <Aux>
         <Toolbar/>
+        {<SideDrawer /> }
         <main className={classes.Content}>
             {props.children}
         </main>
