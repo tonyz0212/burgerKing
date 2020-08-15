709d10bb5dd4215a1fb1f1b0b1f4522bf547c8d6
tony <tonyz0212@gmail.com>
2020-08-15 13:54:08 -0700

    Send order information to server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
     salad: 1.5,
@@ -82,7 +82,22 @@ class BurgerBuilder extends Component {
     }
 
     continuelOrderHandler = () =>{
-        alert('continue');
+        //alert('continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price : this.state.totalPrice,
+            customer: {
+                name: 'Steve ',
+                address:{
+                    street:'Taiwan # one'
+                },
+                email: 'steveRankOne@gmail.com' 
+            },
+            delieveryMethod: 'Airplane'
+        }
+        axios.post('./orders.json',order).then( response => console.log(response))
+            .catch(error => console.log(error ));
+
     }
 
     render() {
